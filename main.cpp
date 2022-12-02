1e7f4e0978bff490c57d4c3b4ddfc5103cdd8a4f
irenemassa03 <114859857+irenemassa03@users.noreply.github.com>
2022-12-02 19:34:13 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+int a ;
+cin >> a;
+if (primo(a)==true){
+cout << "numero primo" <<endl;
+}else{
+cout << "numero non primo" << endl;
+}
   return 0;
 }
